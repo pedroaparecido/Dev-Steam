917767af69883e9029c6a9535c336870e7255991
Pedro Ap <pedroaparecidori@gmail.com>
2024-05-21 14:21:35 -0300

    Criado container e subtitulo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 
 import Navbar from '@/Components/Navbar/Navbar'
+import Subtitle from '@/Components/tipography/subtitle/subtitle'
+import Container from '@/Components/container/container'
 
 export default function Home() {
   return (
@@ -13,6 +15,14 @@ export default function Home() {
       </Head>
       <div>
         <Navbar />
+        <Container>
+          <div>
+            <Subtitle>Promoções</Subtitle>
+          </div>
+          <div>
+            <Subtitle>Outros Jogos</Subtitle>
+          </div>
+        </Container>
       </div>
     </>
   )
