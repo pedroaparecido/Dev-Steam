edc0a85f24ecb63338f17785fe38039665f3fc9f
Pedro Ap <pedroaparecidori@gmail.com>
2024-05-21 15:27:56 -0300

    integrado index com gamecontainer

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
           </div>
           <div className={styles.session}>
             <Subtitle>Outros Jogos</Subtitle>
-            <div>
+            <div className={styles.gamecontainer}>
               <Gamecard />
               <Gamecard />
               <Gamecard />
